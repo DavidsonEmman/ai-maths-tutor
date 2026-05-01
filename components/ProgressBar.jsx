ea30e9be964b4af7fb05badb23d227895ac4bfda
Emmanuel Davidson <emmanueldavidson02@gmail.com>
2026-05-01 20:46:04 +0100

    Add progress bar component to display completion status

--- components/ProgressBar.jsx
+++ components/ProgressBar.jsx
@@ -1,1 +1,10 @@
 
+        {/* PROGRESS BAR */}
+        {totalQ > 0 && phase !== PHASE.SUMMARY && (
+          <div className="prog-bar-wrap">
+            <div className="prog-bar-track">
+              <div className="prog-bar-fill" style={{ width: `${progress}%` }} />
+            </div>
+            <div className="prog-lbl">{qDone}/{totalQ} questions completed</div>
+          </div>
+        )}
